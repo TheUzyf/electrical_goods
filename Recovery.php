bfbb7dd489c56358a6a4e48dd9a8b6bb75add39f
TheUzyf <theuzyf@gmail.com>
2017-05-19 18:24:06 +0500

    Add files via upload

--- Recovery.php
+++ Recovery.php
@@ -1,44 +1,6 @@
-﻿<!DOCTYPE HTML>
+﻿<?php 
+	include ("include/header.php");
+	include ("include/recovform.php");
+	include ("include/footer.php");
+?>
 
-<html>
-
-	<head>
-	
-		<meta charset="UTF-8">
-		
-		<title>
-			Электротовары
-		</title>
-		
-		<link rel="stylesheet" type="text/css" href="Recovery.css" />
-	
-	</head>
-	
-	<body background="Background.png">
-	
-		<header>
-			<a href="Index.html">
-			<img src="Logo.PNG" width="970" alt=""></a>
-		</header>
-		
-		<main>
-		
-			<form method='post' class='reg-form'>
-
-				<div class='form-row'>
-					<label for='form_email'>Email: </label>
-					<input type='email' id='form_email' name='login'>
-				</div>
-								
-				<div class='form-row'>
-					<input type="submit" value='Сбросить пароль'>
-				</div>
-				
-			</form>
-	
-		</main>
-				<footer>
-					© 2017 Электротовары.ру
-                </footer>
-	</body>
-</html>
