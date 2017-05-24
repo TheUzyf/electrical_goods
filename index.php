a3a0b269f0a823001ef44415f0b24524559715d6
TheUzyf <theuzyf@gmail.com>
2017-05-24 19:34:59 +0500

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	include ("include/header.php");
-	include ("include/second_block.php");
-	include ("include/sidebar.php");
-	include ("include/content.php");
-	include ("include/right_column.php");
-	include ("include/footer.php");
-?>
